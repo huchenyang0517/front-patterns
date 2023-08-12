aa805e4505faba10777f1f1d1f825dad45fe6255
huchenyang0517 <cyhu0517@163.com>
2023-08-12 12:04:57 +0800

    add container-presentational-pattern

---
+++ desigin-patterns/container-presentational-pattern.js
@@ -0,0 +1,31 @@
+//Presentational Components: Components that care about how data is shown to the user.
+
+// import React from "react";
+//
+// export default function DogImages({ dogs }) {
+//     return dogs.map((dog, i) => <img src={dog} key={i} alt="Dog" />);
+// }
+
+//Container Components: Components that care about what data is shown to the user.
+
+// import React from "react";
+// import DogImages from "./DogImages";
+//
+// export default class DogImagesContainer extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             dogs: []
+//         };
+//     }
+//
+//     componentDidMount() {
+//         fetch("https://dog.ceo/api/breed/labrador/images/random/6")
+//             .then(res => res.json())
+//             .then(({ message }) => this.setState({ dogs: message }));
+//     }
+//
+//     render() {
+//         return <DogImages dogs={this.state.dogs} />;
+//     }
+// }
