9c0f5d742d80324c3f05193c4c8c9e04f9b9872d
huchenyang0517 <cyhu0517@163.com>
2023-08-13 11:19:00 +0800

    add finegrined

---
+++ desigin-patterns/finegrined.js
@@ -0,0 +1,64 @@
+// state->subs [effect,...]
+// effect{
+//     execute,
+//     deps:(sub,sub...)
+// }
+
+const effectStack = [];
+
+function subscribe(subs, effect) {
+    subs.add(effect);
+    effect.deps.add(subs);
+}
+
+function useState(value) {
+    const subs = new Set();
+
+    const getter = () => {
+        //订阅
+        const effect = effectStack[effectStack.length - 1];
+        subscribe(subs, effect);
+        return value;
+    };
+
+    const setter = (newValue) => {
+        //触发notify
+        value = newValue;
+        for (const effect of [...subs]) {
+            effect.execute();
+        }
+    };
+    return [getter, setter];
+}
+
+function cleanUp(effect) {
+    for (const sub of effect.deps) {
+        sub.delete(effect);
+    }
+    effect.deps.clear();
+}
+
+function useEffect(callback) {
+    const execute = () => {
+        cleanUp(effect);
+        effectStack.push(effect);
+    }
+    try {
+        callback();
+    } finally {
+        effectStack.pop();
+    }
+
+    const effect = {
+        execute,
+        deps: new Set(),
+    }
+}
+
+const [num, setNum] = useState(1);
+
+useEffect(() => {
+    console.log(num())
+})
+
+setNum(2)
