31567c73afd5a3a7126a54397a56d2916ccedcae
huchenyang0517 <cyhu0517@163.com>
2023-08-21 09:39:26 +0800

    add mediator-middleware-pattern

---
+++ desigin-patterns/mediator-middleware-pattern.js
@@ -0,0 +1,23 @@
+class ChatRoom {
+    logMessage(user, message) {
+        const time = new Date();
+        const sender = user.getName();
+
+        console.log(`${time} [${sender}]: ${message}`);
+    }
+}
+
+class User {
+    constructor(name, chatroom) {
+        this.name = name;
+        this.chatroom = chatroom;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    send(message) {
+        this.chatroom.logMessage(this, message);
+    }
+}
