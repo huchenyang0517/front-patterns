c22acb6c40bda73e253ac6859d9e24d55a7a4ee4
huchenyang0517 <cyhu0517@163.com>
2023-08-24 10:11:19 +0800

    add hoc-pattern

--- desigin-patterns/hoc-pattern.js
+++ desigin-patterns/hoc-pattern.js
@@ -1,12 +1,62 @@
-function withStyles(Component) {
-    return props => {
-        const style = {padding: '0.2rem', margin: '1rem'}
-        return <Component style={style} {...props} />
+import React from "react";
+
+//日志记录 HOC
+function withLogging(Component) {
+    return class extends React.Component {
+        componentDidMount() {
+            console.log('Component is mounted');
+        }
+
+        componentWillUnmount() {
+            console.log('Component is unmounted');
+        }
+
+        render() {
+            return <Component {...this.props} />;
+        }
     }
 }
 
-const Button = () = <button>Click me!</button>
-const Text = () => <p>Hello World!</p>
+const MyComponent = () => {
+    return <div>My Component</div>;
+}
 
-const StyledButton = withStyles(Button)
-const StyledText = withStyles(Text)
+const MyComponentWithLogging = withLogging(MyComponent);
+
+//权限控制 HOC
+function withAuthorization(Component, allowedRoles) {
+    return class extends React.Component {
+        render() {
+            const currentUserRole = getCurrentUserRole();
+
+            if (allowedRoles.includes(currentUserRole)) {
+                return <Component {...this.props} />;
+            } else {
+                return null; // 或者显示一个无权限的提示信息
+            }
+        }
+    }
+}
+
+const MyComponent = () => {
+    return <div>My Component</div>;
+}
+
+const AuthorizedComponent = withAuthorization(MyComponent, ['admin', 'manager']);
+
+function withStyles(Component, styles) {
+    return class extends React.Component {
+        render() {
+            const mergedStyles = { ...defaultStyles, ...styles };
+
+            return <Component {...this.props} styles={mergedStyles} />;
+        }
+    }
+}
+
+//样式增强 HOC
+const MyComponent = ({ styles }) => {
+    return <div style={styles}>My Component</div>;
+}
+
+const StyledComponent = withStyles(MyComponent, { color: 'red' });
